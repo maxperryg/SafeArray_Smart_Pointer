ada0c8de5c8867f3d1c01cf722099fb212c350ef
Maxwell <maxperryg@gmail.com>
2018-11-19 18:36:20 -0500

    Everything

--- SafeArray/SafeArray.cpp
+++ SafeArray/SafeArray.cpp
@@ -7,19 +7,35 @@
 //
 
 #include<iostream>
+#include<memory>
 #include <cstdlib>
 #include<cassert>
 using namespace std;
+
+template <class T>
 class SA{
 private:
     int low, high;
-    int* p;
+    unique_ptr<T[]> p;
 public:
     
     // default constructor
     // allows for writing things like SA a;
     
     SA(){low=0; high=-1;p=NULL;}
+    
+    SA(initializer_list<T> h){
+        low = 0;
+        high = (int)h.size()-1;
+        p = unique_ptr<T[]>(new T[(int)h.size()]);
+        int i = 0;
+        for(auto e:h){
+            p[i++] = e;
+        }
+        //p = move(new T[h.size()]);
+        //p=new T[h.size()];
+        
+    }
     
     
     // 2 parameter constructor lets us write
@@ -31,7 +47,8 @@ public:
             exit(1);}
         low=l;
         high=h;
-        p=new int[h-l+1];
+        p = unique_ptr<T[]>(new T[(int)h-l+1]);
+        //p=new T[h-l+1];
     }
     
     
@@ -40,14 +57,16 @@ public:
     // SA x(10); and getting an array x indexed from 0 to 9
     
     SA(int i){low=0; high=i-1;
-        p=new int[i];
+        p = unique_ptr<T[]>(new T[(int)i]);
+        //p=new T[i];
     }
     // copy constructor for pass by value and
     // initialization
     
     SA(const SA & s){
         int size=s.high-s.low+1;
-        p=new int[size];
+        p = unique_ptr<T[]>(new T[(int)size]);
+        //p=new T[size];
         for(int i=0; i<size; i++)
             p[i]=s.p[i];
         low=s.low;
@@ -55,9 +74,9 @@ public:
     }
     // destructor
     
-    ~SA(){
-        delete [] p;
-    }
+//    ~SA(){
+//        delete [] p;
+//    }
     //overloaded [] lets us write
     //SA x(10,20); x[15]= 100;
     
@@ -72,26 +91,28 @@ public:
     // overloaded assignment lets us assign
     // one SA to another
     
-    SA & operator=(const SA & s){
-        if(this==&s)return *this;
-        delete [] p;
-        int size=s.high-s.low+1;
-        p=new int[size];
-        for(int i=0; i<size; i++)
-            p[i]=s.p[i];
-        low=s.low;
-        high=s.high;
-        return *this;
-    }
+//    SA & operator=(const SA & s){
+//        if(this==&s)return *this;
+//        delete [] p;
+//        int size=s.high-s.low+1;
+//        p = move(s.p);
+//        //p=new T[size];
+//        //for(int i=0; i<size; i++)
+//            //p[i]=s.p[i];
+//        low=s.low;
+//        high=s.high;
+//        return *this;
+//    }
     
     // overloads << so we can directly print SAs
-    
-    friend ostream& operator<<(ostream& os, SA s);
+    template <class U>
+    friend ostream& operator<<(ostream& os, SA<U> s);
 };
-ostream& operator<<(ostream& os, SA s){
+
+template <class T>
+ostream& operator<<(ostream& os, SA<T> s){
     int size=s.high-s.low+1;
     for(int i=0; i<size; i++)
         cout<<s.p[i]<<endl;
     return os;
 };
-
--- SafeArray/main.cpp
+++ SafeArray/main.cpp
@@ -7,25 +7,44 @@
 //
 
 #include <iostream>
-
+#include "SafeArray.cpp"
 int main(){
-    SA a(10), b(3,5);
+    SA<int> a(10), b(3,5);
     b[3]=3; b[4]=4; b[5]=5;
     int i;
     for( i=0;i<10;i++)
         a[i]=10-i;
-    cout<<"printing a the first time“ <<endl;
+    cout<<"printing a the first time"<<endl;
     cout<<a<<endl;
     cout<<"printing using []"<<endl;
     for( i=0;i<10;i++)
         cout<<a[i]<<endl;
     // write your own sort
-    Sort(a,10);
-    cout<<"printing a the second time“ <<endl;
-    cout<<a<<endl;
-    b[4]=12;
-    cout<<"printing b “ <<endl;
-    cout<<b<<endl;
-    a[10]=12; // should print an error message and exit
+    
+    //a[10]=12; // should print an error message and exit
+    
+    //New SA Code
+    cout<<"Now to test the Initializer List"<<endl<<endl<<endl;
+    SA<int> s1(10);
+    SA<int> s2(1,6);
+    
+    for(int i=0;i<10;i++){
+        s1[i]=10-i;
+        cout<<s1[i]<<" ";
+    }
+    cout<<endl;
+    
+    for(int i=1;i<=6;i++){
+        s2[i]=i;
+        cout<<s2[i]<<" ";
+    }
+    cout<<endl;
+    
+    SA<int> s3{1,2,3,4,5,6,7,8,9};
+    for(int i=0;i<9;i++){
+        cout<<s3[i]<<" ";
+    }
+    cout<<endl;
+    
     return 0;
 } 
